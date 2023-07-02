97688e5acd7276709daaf044a179633f2d7cdc42
Paryblocks <matheus4steiner9silva@gmail.com>
2023-07-02 15:13:03 -0300

    Biblioteca funcionando

--- src/components/GaleriaFichas.js
+++ src/components/GaleriaFichas.js
@@ -1,56 +1,58 @@
 import { useState, useEffect, useRef } from 'react'
-import { Document, Page } from 'react-pdf'
+import { Document, Page, pdfjs } from 'react-pdf'
 import { useRetrieveDatabase } from '../hooks/useRetrieveDatabase'
 
-import { styles } from './GaleriaFichas.module.css'
+import styles from './GaleriaFichas.module.css'
 
-const GaleriaFichas = () => {
-    const [pdfs, setPdfs] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
+
+const GaleriaFichas = () => { 
+    const [pdfs, setPdfs] = useState([])
+    const [currentPage, setCurrentPage] = useState(0)
     const galleryRef = useRef(null)
     const { getSheets } = useRetrieveDatabase()
 
     useEffect(() => {
         const fetchPdfs = async () => {
-        const listaFichas = await getSheets();
-        setPdfs(listaFichas || []);
+        const listaFichas = await getSheets()
+        setPdfs(listaFichas || [])
         };
 
-        fetchPdfs();
-    }, []);
+        fetchPdfs()
+    }, [])
 
-    const fichasPerPage = 5;
-    const startIndex = currentPage * fichasPerPage;
-    const endIndex = startIndex + fichasPerPage;
-    const currentPdfs = pdfs.slice(startIndex, endIndex);
+    const fichasPerPage = 4
+    const startIndex = currentPage * fichasPerPage
+    const endIndex = startIndex + fichasPerPage
+    const currentPdfs = pdfs.slice(startIndex, endIndex)
 
     const goToNextPage = () => {
-        setCurrentPage((prevPage) => prevPage + 1);
-        galleryRef.current.scrollTo(0, 0);
+        setCurrentPage((prevPage) => prevPage + 1)
+        galleryRef.current.scrollTo(0, 0)
     };
 
     const goToPreviousPage = () => {
-        setCurrentPage((prevPage) => prevPage - 1);
-        galleryRef.current.scrollTo(0, 0);
+        setCurrentPage((prevPage) => prevPage - 1)
+        galleryRef.current.scrollTo(0, 0)
     };
 
     return (
         <div>
-            <div ref={galleryRef}>
+            <div ref={galleryRef} className={styles.container}>
             {currentPdfs.map((pdf) => (
-                <div key={pdf.id}>
-                    <Document file={pdf.sheetURL}>
-                        <Page pageNumber={1} />
-                    </Document>
+                <div key={pdf.id} className={styles.pdf}>
                     <span>{pdf.nome}</span>
+                    <Document file={pdf.sheetURL}>
+                        <Page pageNumber={1} width={200}/>
+                    </Document>
                 </div>
             ))}
             </div>
-            <div>
-                <button onClick={goToPreviousPage} disabled={currentPage === 0}>
+            <div className={styles.buttons}>
+                <button className={styles.butto} onClick={goToPreviousPage} disabled={currentPage === 0}>
                     Anterior
                 </button>
-                <button onClick={goToNextPage} disabled={endIndex >= pdfs.length || 0}>
+                <button className={styles.butto} onClick={goToNextPage} disabled={endIndex >= pdfs.length || 0}>
                     Próxima
                 </button>
             </div>
