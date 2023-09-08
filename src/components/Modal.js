ccedb62ead618b332aa291e8ff9581231cef6812
Paryblocks <matheus4steiner9silva@gmail.com>
2023-09-08 16:45:03 -0300

    Iniciando coleções

---
+++ src/components/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Modal.module.css';
+
+const Modal = ({ isOpen, onClose, fichas }) => {
+  return (
+    isOpen && (
+      <div className={styles.modal}>
+        <div className={styles.modalContent}>
+          <h2>Suas Fichas</h2>
+          {fichas.length > 0 ? (
+            <ul>
+              {fichas.map((ficha) => (
+                <li key={ficha.id}>{ficha.nome}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>Você ainda não tem nenhuma ficha. Adicione uma da biblioteca ou crie uma agora!</p>
+          )}
+          <button onClick={onClose}>Fechar</button>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Modal;
--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import styles from "./Collection.module.css"
+import Modal from "../../components/Modal";
 
 const Collection = () => {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [fichas, setFichas] = useState([]);
+
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className={styles.container}>
         <div className={styles.box}>
             <h1>Coleção</h1>
+        </div>
+        <div className={styles.box}>
+          <button className={styles.big} onClick={handleOpenModal}>Criar nova coleção</button>
+          <Modal isOpen={modalIsOpen} onClose={handleCloseModal} fichas={fichas} />
+        </div>
+        <div className={styles.box}>
+          
         </div>
     </div>
   )
